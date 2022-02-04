d79cf6ca31485f501f41c8de2bdd779f2c337183
Saifuddin sohan <32895869+SaifuddinGsohan@users.noreply.github.com>
2022-02-04 15:16:33 +0600

    cv 23-1-22

--- src/constant.js
+++ src/constant.js
@@ -1,11 +1,11 @@
-export const EMAIL_ID           = 'Sm.sohanbd504@gmail.com';
-export const WEBSITE_LINK       = 'https://www.sohan.me';
+export const EMAIL_ID = 'Sm.sohan586@gmail.com';
+export const WEBSITE_LINK = 'https://www.sohan.me';
 
-export const RESUME_LINK        = 'https://drive.google.com/file/d/1oQgPlLHHwys1Lp6dGrxF-RyyuTxvnnNL/view?usp=sharing';
-export const GITHUB_LINK        = 'https://github.com/SaifuddinGsohan';
-export const LINKEDIN_LINK      = 'https://www.linkedin.com/in/saifuddin-sohan';
-export const FACEBOOK_LINK      = 'https://facebook.com/SaifuddinFsohan';
-export const INSTAGRAM_LINK     = 'https://www.instagram.com/saifuddinsohan';
-export const TWITTER_LINK       = 'https://twitter.com/SaifuddinTsohan';
+export const RESUME_LINK = 'https://drive.google.com/file/d/1UCmBSi7YuozpMZiMPgsvYoOyA_sHfWzf/view?usp=sharing';
+export const GITHUB_LINK = 'https://github.com/SaifuddinGsohan';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/saifuddin-sohan';
+export const FACEBOOK_LINK = 'https://facebook.com/SaifuddinFsohan';
+export const INSTAGRAM_LINK = 'https://www.instagram.com/saifuddin_sohan';
+export const TWITTER_LINK = 'https://twitter.com/SaifuddinTsohan';
 
-export const G_RECAPTA_TOKEN    = '6LcGQncUAAAAAKYAaNg7ciOYFBEGMuaTQWbA8cEl';
+export const G_RECAPTA_TOKEN = '6LcGQncUAAAAAKYAaNg7ciOYFBEGMuaTQWbA8cEl';
